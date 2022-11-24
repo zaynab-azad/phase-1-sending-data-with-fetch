f47df4044fbb286be4310279a28d4d941792dd68
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-24 12:38:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email) {
+  const body = document.querySelector("body");
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      body.append(data.id);
+    })
+    .catch((e) => {
+      body.append(e);
+    });
+}
